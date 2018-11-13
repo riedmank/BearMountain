64789351c89d26c06d3c40f454498a1ceee80d6f
Jeremy Beck <36429023+mtncrawler@users.noreply.github.com>
2018-11-13 10:28:35 -0800

    added dbcontext, data folder, and temp app settings json

---
+++ BearMountain/BearMountain/Data/BearMountainDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BearMountain.Data
+{
+    public class BearMountainDbContext : DbContext
+    {
+        public BearMountainDbContext(DbContextOptions<BearMountainDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
