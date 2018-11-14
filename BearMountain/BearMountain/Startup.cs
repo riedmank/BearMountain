105293a12692d3057b12f1bc12fef7cc675ceb28
Kristofer Riedman <riedmank@hotmail.com>
2018-11-14 11:49:03 -0800

    Uncommented Line

--- BearMountain/BearMountain/Startup.cs
+++ BearMountain/BearMountain/Startup.cs
@@ -55,10 +55,10 @@ namespace BearMountain
             app.UseStaticFiles();
             app.UseMvcWithDefaultRoute();
 
-            //app.Run(async (context) =>
-            //{
-            //    await context.Response.WriteAsync("Hello World!");
-            //});
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Hello World!");
+            });
         }
     }
 }
