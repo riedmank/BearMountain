c0223fca00643fc8c04ab44a9386f32fedbaf6bb
Jeremy Beck <36429023+mtncrawler@users.noreply.github.com>
2018-11-19 09:21:36 -0800

    added outdoor products requirement for claims based policy

---
+++ BearMountain/BearMountain/Models/Handlers/OutdoorProductsRequirement.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BearMountain.Models.Handlers
+{
+    public class OutdoorProductsRequirement : IAuthorizationRequirement
+    {
+        public OutdoorProductsRequirement()
+        {
+
+        }
+    }
+}
