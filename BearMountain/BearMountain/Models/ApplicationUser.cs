6bd80bc095052e8f2c4d21107ab72334fa5e3101
Jeremy Beck <36429023+mtncrawler@users.noreply.github.com>
2018-11-19 09:33:09 -0800

    added enum for interested product for claims based policy

--- BearMountain/BearMountain/Models/ApplicationUser.cs
+++ BearMountain/BearMountain/Models/ApplicationUser.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -11,6 +12,22 @@ namespace BearMountain.Models
         public string FirstName { get; set; }
 
         public string LastName { get; set; }
+
+        public ProductTypes InterestedProduct { get; set; }
+
+        public enum ProductTypes
+        {
+            [Display(Name = "Outdoor Gear")]
+            outdoors,
+            [Display(Name = "Kitchen")]
+            kitchen,
+            [Display(Name = "School")]
+            school,
+            [Display(Name = "Automotive")]
+            automotive,
+            [Display(Name = "Circus")]
+            circus
+        }
 
     }
 }
---
+++ BearMountain/BearMountain/Models/Handlers/OutdoorProductsHandler.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BearMountain.Models.Handlers
+{
+    public class OutdoorProductsHandler : AuthorizationHandler<OutdoorProductsRequirement>
+    {
+        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, OutdoorProductsRequirement requirement)
+        {
+            
+
+            return Task.CompletedTask;
+        }
+    }
+}
