507ff2cad05e10342e993d47d618a98520203690
Jeremy Beck <36429023+mtncrawler@users.noreply.github.com>
2018-11-20 11:18:25 -0800

    added cart interface and service

---
+++ BearMountain/BearMountain/Models/Interfaces/ICart.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BearMountain.Models.Interfaces
+{
+    public interface ICart
+    {
+        Task AddBasketItem(BasketItems item);
+
+        Task RemoveBasketItem(int id);
+
+        Task<List<Product>> GetAllItemsForBasketID(int? id);
+    }
+}
---
+++ BearMountain/BearMountain/Models/Services/CartService.cs
@@ -0,0 +1,13 @@
+﻿using BearMountain.Models.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BearMountain.Models.Services
+{
+    public class CartService : ICart
+    {
+
+    }
+}
