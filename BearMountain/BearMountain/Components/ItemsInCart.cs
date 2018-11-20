10541233d889652e08d4479e2c5ec74b132902b3
Jeremy Beck <36429023+mtncrawler@users.noreply.github.com>
2018-11-20 13:03:20 -0800

    added components folder to contain Items in Cart viewcomponent

---
+++ BearMountain/BearMountain/Components/ItemsInCart.cs
@@ -0,0 +1,19 @@
+﻿using BearMountain.Data;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BearMountain.Components
+{
+    public class ItemsInCart : ViewComponent
+    {
+        private BearMountainDbContext _context;
+
+        public ItemsInCart(BearMountainDbContext context)
+        {
+            _context = context;
+        }
+    }
+}
