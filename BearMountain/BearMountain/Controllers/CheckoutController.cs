827fea60433d2e6fe8ad781e8261bb3353d6396b
Jeremy Beck <36429023+mtncrawler@users.noreply.github.com>
2018-11-27 10:07:36 -0800

    created checkout controller

---
+++ BearMountain/BearMountain/Controllers/CheckoutController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BearMountain.Controllers
+{
+    public class CheckoutController : Controller
+    {
+        [HttpGet]
+        public ActionResult Receipt()
+        {
+            return View();
+        }
+
+        
+    }
+}
